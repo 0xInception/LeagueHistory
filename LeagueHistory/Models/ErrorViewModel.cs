7acfca80304c66da49877f0a077f7dee126d5acc
0xInception <70986768+0xInception@users.noreply.github.com>
2021-12-26 23:54:02 +0200

    initial commit

---
+++ LeagueHistory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeagueHistory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
